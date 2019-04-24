371d176b3915ad48ca0444fbaa8c5bc2d04f37a4
VOID <andreynovorossiysk734@gmail.com>
2019-04-24 12:46:40 +0300

    asdasd

---
+++ precache-manifest.b0b9d3bb352d5a4043a15ad3e1328be3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/RL/robots.txt"
+  },
+  {
+    "revision": "a0c01f2d597aefe8e98d",
+    "url": "/RL/js/chunk-vendors.8f590eb3.js"
+  },
+  {
+    "revision": "8a5a92807a0005e02183",
+    "url": "/RL/js/app.f08f2a6b.js"
+  },
+  {
+    "revision": "193dae9bd9d010cb1e8e17481b2a5916",
+    "url": "/RL/index.html"
+  },
+  {
+    "revision": "a0c01f2d597aefe8e98d",
+    "url": "/RL/css/chunk-vendors.7f9edddd.css"
+  },
+  {
+    "revision": "8a5a92807a0005e02183",
+    "url": "/RL/css/app.e4a334b1.css"
+  }
+];
